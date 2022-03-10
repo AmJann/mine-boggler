06cbcc80881349c384004830d1ab89eb3d8cbddc
amjan <amjan91@gmail.com>
2022-03-09 21:41:52 -0600

    made the bomb png and added event listeners to the squares

---
+++ script.js
@@ -0,0 +1,34 @@
+/* 
+[]add reset button
+[] create classes with styling for character, character-start, 
+character-end, block, bomb, star, and question mark.
+    []character class 
+    []
+    []character-start
+    []character-end
+    []block
+    []bomb
+    []star
+    []question mark
+[]create logic for movement
+  [] add event listeners to divs 
+  []  query selector all 
+  [] use a loop within a loop to create 2d array 
+  [] use rows and column logic (take time to figure this out)
+  [] */
+  const boardArr =[]
+
+  for(let i = 0; i = 25; i ++){
+      for(let j = 0; j = 25; j ++){
+         boardArr.push[j]
+      }
+  }
+  console.log(boardArr)
+const allSquares = document.querySelectorAll('.square')
+
+// allSquares.addEventlistener('click',function(){
+//     square.className = "#"
+// })
+
+
+
